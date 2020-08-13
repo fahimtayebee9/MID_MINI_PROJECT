1778a02ab99a0e68d74cdce3bcb60ba73d0eb0f6
FAHIM TAYEBEE <fahimtayebee9@gmail.com>
2020-08-13 16:37:07 +0600

    User List PhP Layout and Getting Data using DB

--- layouts/user_list.php
+++ layouts/user_list.php
@@ -1,43 +1,59 @@
+<?php
+    include "../php/DB_Config.php";
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+    table,tr,td,th{
+        border: none;
+        border-collapse: collapse;
+        border: 2px solid black;
+    }
+    h3{
+        text-align: center;
+    }
+    </style>
 </head>
 <body>
-    <?php
-        session_start();
-        $file = fopen('../php/user.txt', 'r');
-        $data = fread($file, filesize('../php/user.txt'));
-        $user = explode('|', $data);
-    ?>
     <table>
-        <th>Id</th>
-        <th>Name</th>
-        <th>Email</th>
-        <th>User Type</th>
+        <tr>
+            <td colspan=4><h3>USERS</h3></td>
+        </tr>
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>User Type</th>
+        </tr>
+        
         <?php
-            $i = 0;
-            while( $row = mysqli_fetch_assoc($allStd) ){
+            $query = "SELECT * FROM user_test";
+            $users = mysqli_query($db,$query);
+            while( $row = mysqli_fetch_assoc($users) ){
                 $id       = $row['id'];
                 $name     = $row['name'];
                 $email    = $row['email'];
-                $phone    = $row['phone'];
-                $address  = $row['address'];
-                $joindate = $row['joindate'];
-                $i++;
+                $userType    = $row['userType'];
         ?>
                 <tr>
+                    <td><?php echo $id; ?></td>
                     <td><?php echo $name; ?></td>
                     <td><?php echo $email; ?></td>
-                    <td><?php echo $phone; ?></td>
-                    <td><?php echo $address; ?></td>
-                    <td><?php echo $joindate; ?></td>
+                    <td><?php echo $userType; ?></td>
                 </tr>
 
-        <?php  } 
+        <?php  
+            } 
         ?>
+        <tr>
+            <td colspan=4><a href="admin_home.php">Go Home</a></td>
+        </tr>
     </table>
 </body>
 </html>
