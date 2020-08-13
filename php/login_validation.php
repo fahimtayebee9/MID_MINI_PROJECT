3f8cf087df7c18701ccba23b3973646da94a92e7
FAHIM TAYEBEE <fahimtayebee9@gmail.com>
2020-08-13 16:37:39 +0600

    Login validation Updated

--- php/login_validation.php
+++ php/login_validation.php
@@ -2,32 +2,44 @@
     include "DB_Config.php";
     session_start();
     if(isset($_POST['submit'])){
-        if(empty($_POST['email']) || empty($_POST['password'])){
+        if(empty($_POST['user_id']) || empty($_POST['password'])){
             echo "<h4>Invalid Input</h4>";
         }
         else{
-            $email = $_POST['email'];
+            $id = $_POST['user_id'];
             $password = $_POST['password'];
-            $sql = "SELECT * FROM users where email='$email' and password = '$password'";
+            $sql = "SELECT * FROM user_test where id='$id' and password = '$password'";
             $alluser = mysqli_query($db, $sql);
             $i = mysqli_num_rows($alluser);
             if($i > 0){
                 while( $row = mysqli_fetch_assoc($alluser) ){
-                    $id       = $row['id'];
+                    $id_db       = $row['id'];
                     $email_db    = $row['email'];
-                    $username_db   = $row['username'];
                     $password_db  = $row['password'];
-                    $role = $row['role'];
-                    if(($email == $email_db) && ($password == $password_db)){
+                    $role = $row['userType'];
+                    $name = $row['name'];
+                    if(($id == $id_db) && ($password == $password_db)){
                         if($role == 'Admin'){
-                            $_SESSION['username'] = $username_db;
-                            setcookie('email', $email, time() + (86400 * 30), "/");
-                            setcookie('password', $password, time() + (86400 * 30), "/");
+                            $_SESSION['name'] = $name;
+                            $_SESSION['email'] = $email_db;
+                            $_SESSION['id'] = $id;
+                            $_SESSION['role'] = $role;
+                            if(isset($_POST['remember_me'])){
+                                setcookie('id', $id, time() + (86400 * 30), "/");
+                                setcookie('password', $password, time() + (86400 * 30), "/");
+                            }
                             header('location: ../layouts/admin_home.php');
                             break;
                         }
                         else if($role == 'User'){
-                            $_SESSION['username'] = $username_db;
+                            $_SESSION['name'] = $name;
+                            $_SESSION['email'] = $email_db;
+                            $_SESSION['id'] = $id;
+                            $_SESSION['role'] = $role;
+                            if(isset($_POST['remember_me'])){
+                                setcookie('id', $id, time() + (86400 * 30), "/");
+                                setcookie('password', $password, time() + (86400 * 30), "/");
+                            }
                             header('location: ../layouts/user_home.php');
                             break;
                         }
