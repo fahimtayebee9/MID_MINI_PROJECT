0bbae70c8c5d5781551034bf236689e5f601acc6
FAHIM TAYEBEE <fahimtayebee9@gmail.com>
2020-08-13 16:34:49 +0600

    Login Php Layout Update

--- layouts/login.php
+++ layouts/login.php
@@ -8,70 +8,111 @@ session_start();
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/logRegStyle.css">
     <title>Hotel Management System Portal</title>
+
+    <style>
+        .main-body{
+            margin: auto;
+        }
+        table{
+            margin: auto;
+        }
+        input{
+            padding: 5px 10px;
+            margin-bottom: 10px ;
+        }
+        fieldset{
+            width: fit-content;
+            margin: auto;
+        }
+    </style>
 </head>
 <body>
     <section class="main-body">
-        <div class="container">
-            <div class="row justify-content-center m-auto row-height align-items-center">
-                <div class="col-6 form-background">
-                    <div class="row text-center justify-content-center align-items-center">
-                        <div class="col-12">
-                            <h1>Login To Continue</h1>
-                        </div>
-                    </div>
-                    <?php
-                        if(isset($_COOKIE['email']) && isset($_COOKIE['password'])){
-                            $email = $_COOKIE['email'] ;
-                            $pass = $_COOKIE['password'] ;
-                        }
-                        else{
-                            $email = '' ;
-                            $pass = '';
-                        }
-                    ?>
-                    <form action="../Php/Login_validation.php" method="POST">
-                        <div class="form-group">
-                            <input type="email" name="email" class="form-control" id="Email" aria-describedby="emailHelp" placeholder="Email address" value="<?php echo $email?>">
-                            <?php
-                                if(isset($_SESSION['invalidUser'])){
-                                    ?>
-                                    <p><?php echo $_SESSION['invalidUser'];?></p>
-                            <?php
-                                }
-                            ?>
-                        </div>
-                        <div class="form-group">
-                            <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password" value="<?php echo $pass?>">
-                            <?php
-                                if(isset($_SESSION['invalidPass'])){
-                                    ?>
-                                    <p><?php echo $_SESSION['invalidPass'];?></p>
-                            <?php
-                                }
-                            ?>
-                        </div>
-                        <button type="submit" name="submit" class="btn btn-primary btn-custom">Submit</button>
-                        <div class="error-message">
-                            <h5></h5>
-                        </div>
-                        <div class="reg-area">
-                            <a href="../layouts/registration.php">Don't have an Account? Click Here.</a>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>        
+        <?php
+            if(isset($_COOKIE['id']) && isset($_COOKIE['password'])){
+                $id = $_COOKIE['id'] ;
+                $pass = $_COOKIE['password'] ;
+            }
+            else{
+                $id = '' ;
+                $pass = '';
+            }
+        ?>
+        <fieldset>
+            <legend>Login</legend>
+            <form action="../Php/Login_validation.php" method="POST">
+                <table>
+                    <tr>
+                        <td><label for="">ID </label></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" name="user_id" class="" id="email" aria-describedby="emailHelp" placeholder="User ID" value="<?php echo $id?>"></td>
+                        <?php
+                            if(isset($_SESSION['invalidUser'])){
+                        ?>
+                                <p><?php echo $_SESSION['invalidUser'];?></p>
+                        <?php
+                            }
+                        ?>
+                    </tr>
+                    <tr>
+                        <td><label for="">Password </label></td>
+                    </tr>
+                    <tr>
+                        <td><input type="password" name="password" class="" id="exampleInputPassword1" placeholder="Password" value="<?php echo $pass?>"></td>
+                        <?php
+                            if(isset($_SESSION['invalidPass'])){
+                        ?>
+                                <p><?php echo $_SESSION['invalidPass'];?></p>
+                        <?php
+                            }
+                        ?>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div class="error-message">
+                                <?php
+                                    if(isset($_SESSION['invalidPass'])){
+                                ?>
+                                        <p><?php echo $_SESSION['invalidPass'];?></p>
+                                <?php
+                                    }
+                                    else if(isset($_SESSION['errorMessage'])){
+                                        unset($_SESSION['register']);
+                                        unset($_SESSION['invalidPass']);
+                                ?>
+                                    <p><?php echo $_SESSION['errorMessage'];?></p>
+                                <?php
+                                    }
+                                    else if(isset($_SESSION['register'])){
+                                        unset($_SESSION['errorMessage']);
+                                ?>
+                                        <p><?php echo $_SESSION['register'];?></p>
+                                <?php
+                                    }
+                                ?>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="checkbox" name="remember_me" id=""> Remember Me
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><hr></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="submit" name="submit" class="btn-custom" value="Login">
+                            <a href="../layouts/registration.php">Register</a>
+                        </td>
+                    </tr>
+                </table>
+            </form> 
+        </fieldset>
+        
     </section>
-    
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 </html>
